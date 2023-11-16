45d6a86cce12917a890e1b91c6c798327375cc2e
James Connell <james@Jamess-MacBook-Pro.local>
2023-11-16 11:56:18 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import * as pulumi from "@pulumi/pulumi";
+
+import * as k8s from "@pulumi/kubernetes";
+
+let providerConfig: k8s.ProviderArgs = {};
+
+const config = new pulumi.Config();
+
+if (config.get("renderToDirectory")) {
+  providerConfig = {
+    renderYamlToDirectory: "yaml",
+    ...providerConfig,
+  };
+}
+
+const k8sProvider = new k8s.Provider("yaml-provider", providerConfig);
+
+const pod = new k8s.core.v1.Pod("my-pod", {
+    spec: {
+        containers: [{
+            name: "nginx",
+            image: "nginx:1.14-alpine",
+        }],
+    },
+}, {provider: k8sProvider});  
