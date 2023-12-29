4c2f17cb2c212c18a213f3c75b688f9cca3e42ab
James Connell <jconnell@pulumi.com>
2023-12-29 11:03:09 -0800

    Simplify example further

--- index.ts
+++ index.ts
@@ -1,24 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
-let providerConfig: k8s.ProviderArgs = {};
-
-const config = new pulumi.Config();
-
-if (config.get("renderToDirectory")) {
-  providerConfig = {
-    renderYamlToDirectory: config.get("renderToDirectory"),
-    ...providerConfig,
-  };
-}
+let providerConfig: k8s.ProviderArgs = {
+    renderYamlToDirectory: 'yaml'
+};
 
 const k8sProvider = new k8s.Provider("yaml-provider", providerConfig);
 
 const pod = new k8s.core.v1.Pod("nginx-pod", {
+    metadata: {
+      name: 'my-nginx-pod',
+    },
     spec: {
         containers: [{
             name: "nginx",
-            image: "nginx:1.14-alpine",
+            image: "nginx:1.16-alpine",
         }],
     },
 }, {provider: k8sProvider});  
